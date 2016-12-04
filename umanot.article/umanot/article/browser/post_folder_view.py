524b987a8aa9580efa0952d0a273178193941e6d
akbar <akbar@aebbbefb-ca45-49d7-ba24-901984191f02>
2016-12-04 16:50:29 +0000

    Changed P.F just for a test

--- umanot.article/umanot/article/browser/post_folder_view.py
+++ umanot.article/umanot/article/browser/post_folder_view.py
@@ -160,7 +160,8 @@ class PostFolderView(BrowserView):
                 performance['net_profit_percentuale'] = '+ %.1f %%' % (latest['net_profit'] / float(100000) * 100)
                 performance['drawdown'] = self.context.getLocation()  # latest['drawdown']
                 performance['hit_rate'] = '%0.1f%%' % hit_rate if hit_rate else ''
-                performance['profit_factor'] = '%.1f' % latest['profit_factor'] if latest['profit_factor'] else '--'
+                performance['profit_factor'] = '%.1f' % latest['win_op'] if latest['win_op'] else '--'
+                # performance['profit_factor'] = '%.1f' % latest['profit_factor'] if latest['profit_factor'] else '--'
                 performance['win_op'] = '%0.1f%%' % hit_rate if hit_rate else ''
                 # performance['win_op'] = '%.1f' % latest['win_op'] if latest['win_op'] else '--'
 
@@ -223,7 +224,8 @@ class PostFolderView(BrowserView):
             performance['net_profit_open'] = str(latest['net_profit_open']).split('.')[0] if latest['net_profit_open'] else ''
             performance['drawdown'] = self.context.getLocation()  # latest['drawdown']
             performance['hit_rate'] = '%0.1f %%' % hit_rate if hit_rate else ''
-            performance['profit_factor'] = '%.1f' % latest['profit_factor'] if latest['profit_factor'] else '--'
+            performance['profit_factor'] = '%.1f' % latest['win_op'] if latest['win_op'] else '--'
+            #performance['profit_factor'] = '%.1f' % latest['profit_factor'] if latest['profit_factor'] else '--'
             performance['win_op'] = '%0.1f%%' % hit_rate if hit_rate else ''
             # performance['win_op'] = '%.1f' % latest['win_op'] if latest['win_op'] else '--'
 
