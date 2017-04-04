f9f93ddd58eab5158c0a3ca3979cc3aa362bdea3
akbar <akbar@aebbbefb-ca45-49d7-ba24-901984191f02>
2017-04-04 16:37:30 +0000

    Reverted to old with correct FCA.

--- umanot.article/umanot/article/browser/post_folder_view.py
+++ umanot.article/umanot/article/browser/post_folder_view.py
@@ -148,24 +148,13 @@ class PostFolderView(BrowserView):
             portfolio_sql_id = '0'
             data = self.umanot_utils.get_posts_by_portfolio(portfolio_sql_id, self.limit, self.min_date)
 
-            performance = {'net_profit': None, 'drawdown': None, 'hit_rate': None, 'profit_factor': None, 'total_op': None, 'win_op': None, 'lose_op': None, 'open_op': None, 'net_profit_open': None,}
-            #performance = {'net_profit': None, 'drawdown': None, 'hit_rate': None, 'profit_factor': None, 'total_op': None, 'win_op': None, 'lose_op': None, 'open_op': None,'net_profit_open': None, 'net_profit_2': None, 'drawdown_2': None, 'hit_rate_2': None, 'profit_factor_2': None, 'total_op_2': None, 'win_op_2': None, 'lose_op_2': None, 'open_op_2': None,'net_profit_open_2': None}
+            performance = {'net_profit': None, 'drawdown': None, 'hit_rate': None, 'profit_factor': None, 'total_op': None, 'win_op': None, 'lose_op': None, 'open_op': None,'net_profit_open': None}
             if data:
-                """ latest = data[0]
- latest_2 = data[1]
- latest_3 = data[2]
- latest_4 = data[3]"""
+                latest = data[0]
                 try:
                     hit_rate = float(latest['win_op']) / (float(latest['los_op']) + float(latest['win_op'])) * 100
-                    """hit_rate_2 = float(latest_2['win_op']) / (float(latest_2['los_op']) + float(latest_2['win_op'])) * 100
-hit_rate_3 = float(latest_3['win_op']) / (float(latest_3['los_op']) + float(latest_3['win_op'])) * 100
-hit_rate_4 = float(latest_4['win_op']) / (float(latest_4['los_op']) + float(latest_4['win_op'])) * 100"""
-
                 except:
                     hit_rate = 0
-                    """hit_rate_2 = 0
-hit_rate_3 = 0
-hit_rate_4 = 0"""
 
                 performance['net_profit'] = str(latest['net_profit']).split('.')[0]
                 performance['total_equity'] = str(latest['net_profit'] + 100000).split('.')[0]
@@ -181,23 +170,6 @@ hit_rate_4 = 0"""
                 # Added one more variable(open_op) in order to display them in the site- by Akbar - 16/12/2016
                 performance['open_op'] = latest['open_op'] if latest['open_op'] else '--'
                 performance['net_profit_open'] = str(latest['net_profit_open']).split('.')[0] if latest['net_profit_open'] else ''
-
-                # for STM stock
-
-                """performance['net_profit_2'] = str(latest_2['net_profit_2']).split('.')[0]
-performance['total_equity_2'] = str(latest_2['net_profit_2'] + 100000).split('.')[0]
-performance['net_profit_percentuale_2'] = '+ %.1f %%' % (latest_2['net_profit'] / float(100000) * 100)
-performance['drawdown_2'] = self.context.getLocation()  # latest['drawdown']
-performance['hit_rate_2'] = '%0.1f%%' % hit_rate_2 if hit_rate_2 else ''
-performance['profit_factor_2'] = '%.1f' % latest_2['profit_factor_2'] if latest_2['profit_factor_2'] else '--'
-# Added the following 4 variables in order to display them in the site- by Akbar - 7/12/2016
-# performance['win_op'] = '%.1f' % int(latest['win_op']) if int(latest['win_op']) else '--'
-performance['total_op_2'] = latest_2['tot_op_2'] if latest_2['tot_op_2'] else '--'
-performance['win_op_2'] = latest_2['win_op_2'] if latest_2['win_op_2'] else '--'
-performance['lose_op_2'] = latest_2['los_op_2'] if latest_2['los_op_2'] else '--'
-# Added one more variable(open_op) in order to display them in the site- by Akbar - 16/12/2016
-performance['open_op_2'] = latest_2['open_op_2'] if latest_2['open_op_2'] else '--'
-performance['net_profit_open_2'] = str(latest_2['net_profit_open_2']).split('.')[0] if latest_2['net_profit_open_2'] else ''"""
 
                 last_value = 0
                 counter = 0
@@ -234,25 +206,6 @@ performance['net_profit_open_2'] = str(latest_2['net_profit_open_2']).split('.')
             # Added one more variable(open_op) as text in order to display them in the site- by Akbar - 16/12/2016
             text = text.replace('$OPEN_OP', str(performance['open_op']))
 
-            # for STM stock
-
-            """ text = text.replace('$NET_PROFIT_PERCENTUALE_2', performance['net_profit_percentuale_2'])
- text = text.replace('$NET_PROFIT_OPEN_2', str(performance['net_profit_open_2']))
- text = text.replace('$NET_PROFIT_2', performance['net_profit_2'])
- text = text.replace('$TOTAL_EQUITY_2', performance['total_equity_2'])
- text = text.replace('$DD_MAX_2', performance['drawdown_2'])
- text = text.replace('$HIT_RATE_2', performance['hit_rate_2'])
- text = text.replace('$PROFIT_FACTOR_2', performance['profit_factor_2'])
- # Accepted the following 4 variables as text in order to display them in the site- by Akbar - 7/12/2016
- # text = text.replace('$TOTAL_OP', performance['total_op'])
- text = text.replace('$WIN_OP_2', str(performance['win_op_2']))
- text = text.replace('$LOSE_OP_2', str(performance['lose_op_2']))
-
- text = text.replace('$TOTAL_OP_2', str(performance['total_op_2']))
- text = text.replace('$LOSE_OP_2', str(performance['lose_op_2']))
- # Added one more variable(open_op) as text in order to display them in the site- by Akbar - 16/12/2016
- text = text.replace('$OPEN_OP_2', str(performance['open_op_2']))"""
-
             placeholder['operazioni'] = placeholder['operazioni'].replace('$TOTAL_OP', str(performance['total_op']))
             placeholder['operazioni_12'] = placeholder['operazioni_12'].replace('$WIN_OP', str(performance['win_op'])).replace('$LOSE_OP', str(performance['lose_op']))
             placeholder['operazioni_1x2'] = placeholder['operazioni_1x2'].replace('$OPEN_OP', str(performance['open_op']))  # @ AKBAR PUT THE OPEN_OP REPLACEMENTE HERE. REMEMBER TO CAST NUMBER TO STRING
@@ -260,17 +213,6 @@ performance['net_profit_open_2'] = str(latest_2['net_profit_open_2']).split('.')
             placeholder['operazioni_1x4'] = placeholder['operazioni_1x4'].replace('$DD_MAX', str(performance['drawdown']))  # 26/3/17 @ Added by AKBAR for enhancing the blue box
             placeholder['operazioni_1x5'] = placeholder['operazioni_1x5'].replace('$PROFIT_FACTOR', str(performance['profit_factor']))  # 26/3/17 @ Added by AKBAR for enhancing the blue box
             placeholder['operazioni_1x6'] = placeholder['operazioni_1x6'].replace('$HIT_RATE', str(performance['hit_rate']))  # 26/3/17 @ Added by AKBAR for enhancing the blue box
-
-            # for STM stock
-
-            """placeholder['operazioni_2'] = placeholder['operazioni_2'].replace('$TOTAL_OP_2', str(performance['total_op_2']))
-placeholder['operazioni_22'] = placeholder['operazioni_22'].replace('$WIN_OP_2', str(performance['win_op_2'])).replace('$LOSE_OP_2', str(performance['lose_op_2']))
-placeholder['operazioni_2x2'] = placeholder['operazioni_2x2'].replace('$OPEN_OP_2', str(performance['open_op_2']))  # @ AKBAR PUT THE OPEN_OP REPLACEMENTE HERE. REMEMBER TO CAST NUMBER TO STRING
-placeholder['operazioni_2x3'] = placeholder['operazioni_2x3'].replace('$NET_PROFIT_2', str(performance['net_profit_2']))  # 26/3/17 @ Added by AKBAR for enhancing the blue box
-placeholder['operazioni_2x4'] = placeholder['operazioni_2x4'].replace('$NET_PROFIT_OPEN_2', str(performance['net_profit_open_2']))  # 26/3/17 @ Added by AKBAR for enhancing the blue box
-placeholder['operazioni_2x5'] = placeholder['operazioni_2x5'].replace('$DD_MAX_2', str(performance['drawdown_2']))  # 26/3/17 @ Added by AKBAR for enhancing the blue box
-placeholder['operazioni_2x6'] = placeholder['operazioni_2x6'].replace('$PROFIT_FACTOR_2', str(performance['profit_factor_2']))  # 26/3/17 @ Added by AKBAR for enhancing the blue box
-placeholder['operazioni_2x7'] = placeholder['operazioni_2x7'].replace('$HIT_RATE_2', str(performance['hit_rate_2']))"""  # 26/3/17 @ Added by AKBAR for enhancing the blue box
 
             info = dict(
                 text = text,
